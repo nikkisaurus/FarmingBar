52f3b83aa0247832a8326fa5a2437d060f517772
Niketa <niketa.wow@gmail.com>
2022-09-11 05:30:58 -0500

    Disable debug

--- Modules/Database/Database.lua
+++ Modules/Database/Database.lua
@@ -463,7 +463,7 @@ function private:InitializeDatabase()
         global = {
             debug = {
                 enabled = false,
-                enabled = true,
+                -- enabled = true,
             },
             settings = {
                 alerts = {
